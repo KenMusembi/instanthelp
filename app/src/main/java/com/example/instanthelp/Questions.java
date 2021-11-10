29860cfcff50e6365f55253354ec926ce7cf13c7
Kennedy Musembi <kenmusembi212gmail.com>
2021-11-10 20:52:17 +0100

    insert questions to firestore database

---
+++ app/src/main/java/com/example/instanthelp/Questions.java
@@ -0,0 +1,14 @@
+package com.example.instanthelp;
+
+public class Questions {
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Questions(String question) {
+        this.question = question;
+    }
+
+    String question;
+}
