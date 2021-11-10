eb42cbd886b8fc2d1157960cabd2cb003d7bec8c
KenMusembi <kenmusembi21@gmail.com>
2021-11-10 20:52:17 +0100

    insert questions to firestore database

--- app/src/main/java/com/example/instanthelp/MainActivity.java
+++ app/src/main/java/com/example/instanthelp/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogOut;
     FirebaseAuth mAuth;
+
+    EditText etAskQuestion;
+    Button buttonSubmitQuestion;
+
+    DatabaseReference questionDBRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
 
+        etAskQuestion = findViewById(R.id.etAskQuestion);
+        buttonSubmitQuestion = findViewById(R.id.buttonSubmitQuestion);
+
+        questionDBRef = FirebaseDatabase.getInstance().getReference().child("Questions");
+
+        buttonSubmitQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertQuestionData();
+            }
+        });
+
+    }
+
+    private void insertQuestionData() {
+        String question = etAskQuestion.getText().toString();
+
+        Questions questions = new Questions(question);
+
+        questionDBRef.push().setValue(questions);
+        Toast.makeText(MainActivity.this, "Question Added", Toast.LENGTH_SHORT).show();
     }
 
     @Override
